c5586825bbcaeedc8985d8f0fa45fadfd9b46a76
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-08-06 20:29:44 +0200

    std::cout and random number guessing

--- exercises.cpp
+++ exercises.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout << "Hello X1 Carbon" << std::endl;
+    std::cout << "Gues a number in the range 0-99" << std::endl;
+
+    srand (time(NULL));   // initialize random seed
+
+    int myNumber = rand() % 100;
+    int yourNumber = 100;
+
+    do
+    {
+        std::cout << "Give me a number: ";
+        std::cin >> yourNumber;
+        if(yourNumber > myNumber)
+        {
+            std::cout << "your number is too big" << std::endl;
+        }
+        else if(yourNumber < myNumber)
+        {
+            std::cout << "your number is too little" << std::endl;
+        }
+    } while(myNumber != yourNumber);
+
+    std::cout << "Congratulations! You have guessed the number " << myNumber << std::endl;
 
     return 0;
 }
