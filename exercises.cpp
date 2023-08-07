4dbe832e6029a0a08005db2b5e98b2b675e9d3e1
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-08-07 20:18:28 +0200

    brace initializer

--- exercises.cpp
+++ exercises.cpp
@@ -4,5 +4,10 @@ int main()
 {
     std::cout << "Hello X1 Carbon" << std::endl;
 
+    auto f1{1.0F};
+    auto d1{1.0};
+
+    std::cout << f1 << " == " << d1 << " ? ... " << ((f1 == d1) ? "true" : "false");
+
     return 0;
 }
