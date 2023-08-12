95a68dc110dc9a59d70a8a3a5e966a14aec199e9
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-08-12 19:23:14 +0200

    arrays and std::array(s)

--- exercises.cpp
+++ exercises.cpp
@@ -1,9 +1,40 @@
 #include "extra.h"
+#include <iostream>
+#include <array>
 
 int main()
 {
-    ns1::hello();
-    ns2::hello();
+    int myArray[] {1,2,3};
+
+    std::cout << "Number of elements = " << ArraySize(myArray) << std::endl;
+
+    for(auto& element : myArray)
+    {
+        if(element == 2)
+        {
+            element = 100;
+        }
+
+        std::cout << element << ",";
+    }
+    std::cout << std::endl;
+
+    //another way of displaying number of elements
+    std::cout << "Number of elements = " << std::size(myArray) << std::endl;
+
+    const size_t mySize{4};
+    std::array<int, mySize> myStdArray{10,11,12,13};
+    for(auto& element : myStdArray)
+    {
+        if(element == 12)
+        {
+            element = 1234;
+        }
+
+        std::cout << element << ",";
+    }
+    std::cout << std::endl;
+    std::cout << "myStdArray size = " << myStdArray.size() << std::endl;
 
     return 0;
 }
--- extra.h
+++ extra.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define ArraySize(array)    (sizeof(array) / sizeof(array[0]))
+
 namespace ns1
 {
     void hello();
