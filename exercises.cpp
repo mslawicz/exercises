08d73b4c15ed0a0346246360fd12341165709476
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-08-12 20:32:17 +0200

    sorting arrays

--- exercises.cpp
+++ exercises.cpp
@@ -1,12 +1,44 @@
 #include "extra.h"
 #include <iostream>
-#include <array>
 
 int main()
 {
-    int myArray[] {1,2,3};
-    auto [x, y, z] = myArray;
-    std::cout << x << "," << y << "," << z << std::endl;
+    constexpr size_t Size{80};
+    int myArray[Size];
+    for(auto& elem : myArray)
+    {
+        elem = rand() % 10;
+        std::cout << elem << ",";
+    }
+    std::cout << std::endl;
+
+    int first{0};
+    int steps{0};
+    while(first < Size - 1)
+    {
+        if(myArray[first] <= myArray[first+1])
+        {
+            first++;
+        }
+        else
+        {
+            int temp = myArray[first+1];
+            myArray[first+1] = myArray[first];
+            myArray[first] = temp;
+            if(first>0)
+            {
+                first--;
+            }
+        }
+        steps++;
+    }
+
+    for(auto elem : myArray)
+    {
+        std::cout << elem << ",";
+    }
+    std::cout << std::endl;
+    std::cout << "steps=" << steps << std::endl;        
     
     return 0;
 }
