753496b1bce4372caccfdf3885f5b7fa9d7bd893
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-08-15 19:10:22 +0200

    raw string

--- exercises.cpp
+++ exercises.cpp
@@ -1,29 +1,9 @@
 #include "extra.h"
 #include <iostream>
-#include <memory>
 
 int main()
 {
-    std::unique_ptr<int> pX(new int);
-
-    *pX = 100;
-    *pX += 2;
-    std::cout << "*" << pX.get() << "==" << *pX << std::endl;
-
-    // aritmetic operations not available: pX+=10;
-
-    //another way of creating unique pointer:
-    auto pY = std::make_unique<int>();
-    // cannot assign a new value: pY = pX;
-    auto myArray = std::make_unique<float[]>(10);
-    myArray[9] = 123.45;
-
-    //no need to delete the pointer allocated memory
-
-    //shared pointer:
-    auto pA = std::make_shared<int>();
-    auto pB(pA);
-    std::cout << pA.get() << "==" << pB.get() << std::endl;
-
+    std::string xstr = R"(C:\temp\x)";
+    std::cout << "path=" << xstr;
     return 0;
 }
