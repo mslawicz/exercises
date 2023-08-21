5fb418d848521435bea34657337eab5d11e91e4c
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-08-21 21:08:25 +0200

    writing to a text file

--- exercises.cpp
+++ exercises.cpp
@@ -1,51 +1,24 @@
 #include "extra.h"
 #include <iostream>
+#include <fstream>
 
-struct Figure
-{
-    std::string color;
-    unsigned int area;
-    bool isBig()
-    {
-        return area > 150 ? true : false;
-    }
-    bool isBigger(const Figure& other)
-    {
-        return area > other.area;
-    }
-    // overloading operator within a structure
-    // bool operator==(const Figure& other) const
-    // {
-    //     return (color == other.color) && (area == other.area);
-    // }
-};
-
-//overloading operator outside of any structure
-bool operator==(const Figure& first, const Figure& second)
-{
-    return (first.color == second.color) && (first.area == second.area);
-}
-
-//overloading stream insertion
-std::ostream& operator<<(std::ostream& stream, const Figure& figure)
-{
-    stream << figure.color << "," << figure.area;
-    return stream;
-}
 
 int main()
 {
-    Figure rectangle{"yellow", 200};
-    Figure triangle{.color = "red", .area = 100};
-
-    //structured binding
-    auto [myColor, myArea] = triangle;
-    std::cout << "color=" << myColor << std::endl;
-    std::cout << "is it big? " << std::boolalpha << triangle.isBig() << std::endl;
-    std::cout << "is it bigger? " << std::boolalpha << triangle.isBigger(rectangle) << std::endl;
-    auto copyOfTriangle{triangle};
-    std::cout << "are they equal? " << std::boolalpha << (triangle == copyOfTriangle) << std::endl;
-    std::cout << triangle << std::endl; 
+    std::ofstream myFile;
+    myFile.open("C:/temp/myFile.txt");
+    if(myFile.is_open())
+    {
+        std::string myText{"my text written to my file\n"};
+        //first version: myFile.write(myText.c_str(), myText.length());
+        myFile << myText;   //second version
+        myFile.close();
+    }
+    else
+    {
+        std::cout << "file open error" << std::endl;
+    }
+    
 
     return 0;
 }
