2c7100345fb4bbf8f32a3eae256375916215dbe4
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-11-01 19:39:31 +0100

    implicite conversion

--- exercises.cpp
+++ exercises.cpp
@@ -1,38 +1,21 @@
 #include "extra.h"
 #include <iostream>
-#include <fstream>
 
+class MyClass
+{
+public:
+    MyClass(int x) {this->X = x;}
+    int X{0};
+};
+
+void showX(MyClass myClass)
+{
+    std::cout << "X = " << myClass.X << std::endl;
+}
 
 int main()
 {
-    std::ofstream myFile;
-    myFile.open("C:/temp/myFile.bin", std::ios::binary);
-    if(myFile.is_open())
-    {
-        double PI = 3.14159;
-        myFile.write(reinterpret_cast<char*>(&PI), sizeof(PI));
-        myFile.close();
-    }
-    else
-    {
-        std::cout << "file open error" << std::endl;
-    }
-
-    //read the created file
-    std::ifstream file2read;
-    file2read.open("C:/temp/myFile.bin", std::ios::binary);
-     if(file2read.is_open())
-    {
-        double myDouble{0};
-        file2read.read(reinterpret_cast<char*>(&myDouble), sizeof(myDouble));
-        std::cout << "the value read from a file: " << myDouble;
-        file2read.close();
-    }
-    else
-    {
-        std::cout << "file open for reading error" << std::endl;
-    }   
-    
+    showX(10);  // implicite conversion!
 
     return 0;
 }
