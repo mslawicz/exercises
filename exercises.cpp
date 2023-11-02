b9d6aff1407ef25125da16db5aec368abfb6f44b
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-11-02 19:17:18 +0100

    copy constructor

--- exercises.cpp
+++ exercises.cpp
@@ -5,29 +5,30 @@ class MyClass
 {
 public:
     explicit MyClass(int x) {this->X = x;}
-    MyClass(int x, int y);
-    void showXY();
+    MyClass(const MyClass& source)    // copy constructor
+    {
+        this->X = source.X * 2;
+    }
+    void showX();
 private:    
     int X{0};
-    int Y{0};
 };
+
+void myFunction(MyClass copiedObject)    // object copy is being made here
+{
+    copiedObject.showX();
+}
 
 
 int main()
 {
-    MyClass myClass(10, 20);
-    myClass.showXY();
+    MyClass myClass(10);
+    myFunction(myClass);    // this object is being copied
 
     return 0;
 }
 
-MyClass::MyClass(int x, int y) :
-    MyClass(x)  // delegated constructor
+void MyClass::showX()
 {
-    this->Y = y;
-}
-
-void MyClass::showXY()
-{
-    std::cout << "X = " << X << "  Y = " << Y << std::endl;
+    std::cout << "X = " << X << std::endl;
 }
