e76df3ef690f810bb8ea78b26300d0250fdfb3f4
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-11-03 19:40:01 +0100

    static member inline initialization

--- exercises.cpp
+++ exercises.cpp
@@ -4,31 +4,14 @@
 class MyClass
 {
 public:
-    explicit MyClass(int x) {this->X = x;}
-    MyClass(const MyClass& source)    // copy constructor
-    {
-        this->X = source.X * 2;
-    }
-    void showX();
-private:    
-    int X{0};
+    inline static int myInt = 100;
 };
-
-void myFunction(MyClass copiedObject)    // object copy is being made here
-{
-    copiedObject.showX();
-}
-
 
 int main()
 {
-    MyClass myClass(10);
-    myFunction(myClass);    // this object is being copied
+    MyClass::myInt++;
+    std::cout << "myInt = " << MyClass::myInt << std::endl;
 
     return 0;
 }
 
-void MyClass::showX()
-{
-    std::cout << "X = " << X << std::endl;
-}
