ba48ea4b3ac0a62b970ab4842b7b0a7eff699878
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-11-07 19:11:30 +0100

    equality operator definition/overloading

--- exercises.cpp
+++ exercises.cpp
@@ -1,17 +1,41 @@
 #include "extra.h"
 #include <iostream>
 
-class MyClass
+class Point
 {
 public:
-    inline static int myInt = 100;
+    Point(int x, int y);
+    bool operator==(const Point& other) const;
+    bool operator!=(const Point& other) const;
+private:
+    int x;
+    int y;    
 };
 
 int main()
 {
-    MyClass::myInt++;
-    std::cout << "myInt = " << MyClass::myInt << std::endl;
+    Point A(1,2);
+    Point B(3,4);
+
+    std::cout << std::boolalpha << (A == B) << std::endl;
+    std::cout << std::boolalpha << (A != B) << std::endl;
 
     return 0;
 }
 
+Point::Point(int x, int y) :
+    x(x),
+    y(y)
+    {
+
+    }
+
+bool Point::operator==(const Point& other) const
+{
+    return (x == other.x) && (y == other.y);
+}
+
+bool Point::operator!=(const Point& other) const
+{
+    return !(*this == other);   //not-equality uses equality operator
+}
