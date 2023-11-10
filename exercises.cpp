baedd3610191af61be771c88fe28cc4e3d4a614c
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-11-10 18:32:53 +0100

    overloading arithmetic operators

--- exercises.cpp
+++ exercises.cpp
@@ -8,6 +8,7 @@ public:
     Point(int x, int y);
     bool operator==(const Point& other) const;
     bool operator!=(const Point& other) const;
+    Point& operator+(const Point& other);
 private:
     int x;
     int y;    
@@ -24,6 +25,7 @@ int main()
     std::cout << std::boolalpha << (A == B) << std::endl;
     std::cout << std::boolalpha << (A != B) << std::endl;
     std::cout << "my points: " << A << " , " << B << std::endl;
+    std::cout << "addition A+B: " << (A+B) << std::endl;
 
     return 0;
 }
@@ -43,6 +45,13 @@ bool Point::operator==(const Point& other) const
 bool Point::operator!=(const Point& other) const
 {
     return !(*this == other);   //not-equality uses equality operator
+}
+
+Point &Point::operator+(const Point &other)
+{
+    x += other.x;
+    y += other.y;
+    return *this;
 }
 
 //this function is a friend of Point class
