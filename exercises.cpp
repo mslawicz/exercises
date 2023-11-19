c3d953ec647507b14fb5d50a0b4959907a852054
Marcin Slawicz <marcin.slawicz@gmail.com>
2023-11-19 19:04:50 +0100

    3 level inheritance

--- exercises.cpp
+++ exercises.cpp
@@ -4,6 +4,7 @@
 class Figure
 {
 public:
+    Figure();
     virtual float getArea() const { return 0.0f; }
     const float PI = 3.1415926f;
 };
@@ -11,7 +12,7 @@ public:
 class Circle : public Figure
 {
 public:
-    Circle(float radius);
+    explicit Circle(float radius);
     float getArea() const;
 private:
     float radius;
@@ -27,19 +28,34 @@ private:
     float B;
 };
 
+class Square : public Rectangle //square is a special case of a rectangle
+{
+public:
+    explicit Square(float A);
+private:
+    float A;
+};
+
 int main()
 {   
     Circle circle1{1.0f};
     Rectangle rectangle1{2.0f, 3.0f};
+    Square square1{2.0f};
     std::cout << "area of the circle = " << circle1.getArea() << std::endl;
     std::cout << "area of the rectangle = " << rectangle1.getArea() << std::endl;
+    std::cout << "area of the square = " << square1.getArea() << std::endl;
     return 0;
+}
+
+Figure::Figure()
+{
+    std::cout << "Figure constructor is called" << std::endl;
 }
 
 Circle::Circle(float radius) :
     radius(radius)
 {
-
+    std::cout << "Circle constructor is called" << std::endl;
 }
 
 float Circle::getArea() const
@@ -51,10 +67,16 @@ Rectangle::Rectangle(float A, float B) :
     A(A),
     B(B)
 {
-
+    std::cout << "Rectangle constructor is called" << std::endl;
 }
 
 float Rectangle::getArea() const
 {
     return A * B;
+}
+
+Square::Square(float A) :
+    Rectangle(A, A)     //the Square constructor must call the Rectangle constructor first
+{
+    std::cout << "Square constructor is called" << std::endl;
 }
