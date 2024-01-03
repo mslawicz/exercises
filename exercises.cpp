209ad684084ee79217bfc6eaac2b2d76c97e722c
Marcin Slawicz <marcin.slawicz@etteplan.com>
2024-01-03 08:29:30 +0100

    explicit type arguments in template functions

--- exercises.cpp
+++ exercises.cpp
@@ -9,8 +9,8 @@ T max(T first, T second)
 
 int main()
 {   
-    std::cout << "max of int: " << max(2, 3) << std::endl;
-    std::cout << "max of float: " << max(2.5f, 2.3f) << std::endl;
-    std::cout << "max of char: " << max('a', 'A') << std::endl;
+    std::cout << "max of double: " << max<double>(2, 3) << std::endl;
+    std::cout << "max of int: " << max<int>(2.1f, 1.9f) << std::endl;
+    std::cout << "max of int: " << max<int>('a', 'A') << std::endl;
     return 0;
 }
