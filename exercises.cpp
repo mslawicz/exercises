6d70f44e49c02c4fcf7954fa0c5a885e7de7f08e
Marcin Slawicz <marcin.slawicz@etteplan.com>
2024-01-04 10:38:44 +0100

    class template

--- exercises.cpp
+++ exercises.cpp
@@ -3,5 +3,9 @@
 
 int main()
 {   
+    Item<std::string, int> item1("level", 1);
+    Item<int, double> item2(100, 0.707);
+    std::cout << "item1 key: " << item1.getKey() << std::endl;
+    std::cout << "item2 value: " << item2.getValue() << std::endl;
     return 0;
 }
--- extra.h
+++ extra.h
@@ -1,1 +1,23 @@
 #pragma once
+
+template<typename K, typename V>
+class Item
+{
+    public:
+    Item(K key, V value);
+    K getKey() const { return key; }
+    V getValue() const { return value; }
+
+    private:
+    K key;
+    V value;
+};
+
+//implementation must be placed in the same module
+template<typename K, typename V>
+Item<K, V>::Item(K key, V value) :
+    key(key),
+    value(value)
+{
+    
+}
